44f1b56f438fc6dbb573bd32ca0efeb929286d36
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-12-26 15:46:23 +0530

    public path change

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8000/",
+    publicPath: "https://micro-frontend-host-app-amber.vercel.app/",
   },
 
   resolve: {
